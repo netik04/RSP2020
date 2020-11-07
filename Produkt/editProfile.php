ed98f0f15c5bb64e927c1498a764910e4cc5bbc6
Jakub Pojer <72390860+CptUnreliable@users.noreply.github.com>
2020-11-07 13:43:03 +0100

    pridani potvrzeni noveho hesla pri jeho zmene

--- Produkt/editProfile.php
+++ Produkt/editProfile.php
@@ -34,16 +34,43 @@
     <fieldset>
         <legend>Změna hesla</legend>
         <form action="scripty/zmenitHeslo.php" method="POST" enctype="multipart/form-data">
-            <label for="newPassword">Nové heslo:</label><input type="password" name="newPassword"/><br/>
+            <label for="newPassword">Nové heslo:</label><input type="password" name="newPassword" id="newPassword" required/> <br/>
+            <label for="newPassword">Potvrdit Nové heslo:</label><input type="password" name="new_passwd_potvrdit" id="new_passwd_potvrdit" required/> <span id="error_passwd"></span><br/>
             <br /><label for="login">Zadejte svoje staré heslo pro potvrzeni zmen</label><br />
             <input type="password" name="oldPassword"/><br/>
-            <input type="submit" name="submit" id="submit" value="Změnit heslo" />
+            <input type="submit" name="submit" id="pass_submit" value="Změnit heslo" />
             <span class="error"><?php echo $_SESSION["error_edit_pass"]; unset($_SESSION["error_edit_pass"]) //zobrazení chyb z přihlášení pomoci session, nasledně unset této session aby se zobrazila pouze jednou?></span>
         </form>
     </fieldset>
 
-    
-
+    <script>
+        $(document).ready(function(){
+            //$("#pass_submit").prop("disabled", true)
+            $("#new_passwd_potvrdit").keyup(function() // pokud uživatel zadal podruhé heslo
+            {
+                if($("#new_passwd_potvrdit").val() == "") // pokud nic nevyplnil
+                {
+                    // není potřeba prozatím nic zobrazovat
+                    $("#error_passwd").text("");
+                    $("#pass_submit").prop("disabled", true);
+                }
+                else if($("#new_passwd_potvrdit").val() != $("#newPassword").val()) // pokud se hesla neshodují
+                {
+                    // upozorni ho na to
+                    $("#error_passwd").text(" Zadaná hesla se musí shodovat!");
+                    $("#error_passwd").css("color", "red");
+                    $("#pass_submit").prop("disabled", true);
+                }
+                else // pokud se shodují
+                {
+                    // vše je v pořádku - zobraz 'checkmark'
+                    $("#error_passwd").html("&nbsp; &#x2713;");
+                    $("#error_passwd").css("color", "green");
+                    $("#pass_submit").prop("disabled", false);
+                }
+            });  
+        });   
+    </script>
 
 <?php    
     }
